247457e251b3083768e73ecde26920a407aa2b64
jserme <dev.hubo@gmail.com>
2016-02-23 22:56:07 +0800

    fix: markdown style, include cube type.css

--- css/build-css-min.sh
+++ css/build-css-min.sh
@@ -1,2 +1,2 @@
 #需要安装 cssmin  npm install -g cssmin
-cat cube-min-v1.3.1.css syntax.css app.css | cssmin > app-min.css
+cat cube-min-v1.3.1.css type-min-v1.3.1.css syntax.css app.css | cssmin > app-min.css
